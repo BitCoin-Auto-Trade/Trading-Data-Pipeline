ef5ef1b10281ab16b16fc8e85051bd69a0e7806e
이승준 <127376832+hhee4455@users.noreply.github.com>
2025-06-19 14:42:50 +0900

    Refactor : redis 통신 오류 해결

--- src/collector/redis_client.py
+++ src/collector/redis_client.py
@@ -2,8 +2,9 @@ import os
 import redis
 
 redis_client = redis.Redis(
-    host=os.getenv("REDIS_HOST", "redis"),
+    host=os.getenv("REDIS_HOST", "localhost"),
     port=int(os.getenv("REDIS_PORT", 6379)),
+    password=os.getenv("REDIS_PASSWORD", None),
     db=0,
     decode_responses=True,
 )
