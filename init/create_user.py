3e61de0cd7025f04b27027a1fb066e92a70ad30b
이승준 <127376832+hhee4455@users.noreply.github.com>
2025-06-24 15:51:58 +0900

    Refactor : 버전 오류 수정

--- init/create_user.py
+++ init/create_user.py
@@ -1,24 +1,33 @@
 from airflow import settings
-from airflow.auth.managers.fab.models import User, Role
+from flask_appbuilder.security.sqla.models import User, Role
 from werkzeug.security import generate_password_hash
 
-session = settings.Session()
-admin_role = session.query(Role).filter_by(name="Admin").first()
-if not admin_role:
-    admin_role = Role(name="Admin")
-    session.add(admin_role)
-    session.commit()
+def create_admin_user():
+    session = settings.Session()
 
-if not session.query(User).filter_by(username="admin").first():
-    user = User(
-        username="admin",
-        email="admin@example.com",
-        is_active=True,
-        password=generate_password_hash("admin"),
-        roles=[admin_role],
-    )
-    session.add(user)
-    session.commit()
-    print("✅ admin 사용자 생성 완료")
-else:
-    print("⚠️ admin 사용자가 이미 존재합니다.")
+    # Admin 역할 가져오기
+    admin_role = session.query(Role).filter_by(name="Admin").first()
+    if not admin_role:
+        admin_role = Role(name="Admin")
+        session.add(admin_role)
+        session.commit()
+
+    # 사용자 존재 확인
+    if not session.query(User).filter_by(username="admin").first():
+        user = User(
+            username="admin",
+            first_name="admin",
+            last_name="admin",
+            email="admin@example.com",
+            active=True,
+            password=generate_password_hash("admin"),
+            roles=[admin_role]
+        )
+        session.add(user)
+        session.commit()
+        print("✅ admin 사용자 생성 완료")
+    else:
+        print("⚠️ 이미 admin 사용자가 존재합니다.")
+
+if __name__ == "__main__":
+    create_admin_user()
